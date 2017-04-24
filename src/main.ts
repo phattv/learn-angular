75a79e9da324e67d542bea1113674adb66a48235
Phat <vanphat@siliconstraits.com>
2017-04-24 15:46:39 +0700

    enableProdMode() if not localhost

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
+
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
